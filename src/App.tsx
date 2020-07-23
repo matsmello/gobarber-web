1f48d1abdcd5e02253591dbe726b4bd295cd4de4
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-23 13:14:23 -0300

    Create default style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import GlobalStyle from "./styles/global";
 
 const App = () => {
-  return <h1>Principal</h1>
-}
+  return (
+    <>
+      <h1>Principal</h1>
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
