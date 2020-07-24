0271a1fa433960aee2f3c48d7bea49a29f4df899
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-24 17:12:48 -0300

    Create signin validation

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SignUp from './pages/SignUp'
 const App = () => {
   return (
     <>
-      <SignUp />
+      <SignIn />
       <GlobalStyle />
     </>
   );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, {useRef, useCallback} from "react";
 import { Container, Content, Background } from "./styles";
 import logoImg from "./../../assets/logo.svg";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 import Input from './../../components/Input'
 import Button from './../../components/Button'
+import {Form} from '@unform/web';
+import {FormHandles} from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from './../../utils/getValidationsErrors'
 
-const SignIn: React.FC = () => (
-  <Container>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object): Promise<void> => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object({
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (<Container>
     <Content>
       <img src={logoImg} alt="GoBarber" />
-      <form>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <h1>Faça seu logon</h1>
 
         <Input icon={FiMail} name="email" placeholder="E-mail" />
@@ -17,7 +42,7 @@ const SignIn: React.FC = () => (
 
         <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
-      </form>
+      </Form>
       <a href="">
         <FiLogIn />
         Criar conta
@@ -26,6 +51,6 @@ const SignIn: React.FC = () => (
 
     <Background />
   </Container>
-);
+)};
 
 export default SignIn;
