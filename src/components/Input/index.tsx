0579d7a3529ca164f74d8363ba9091efdcfcb9b5
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-24 15:04:50 -0300

    Integrate unform to our signup form

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,28 @@
-import React, { InputHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes, useRef, useEffect } from 'react'
 import {Container} from './styles'
 import {IconBaseProps} from 'react-icons';
+import {useField} from '@unform/core';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({icon: Icon, ...props}) => {
+const Input: React.FC<InputProps> = ({name, icon: Icon, ...props}) => {
+  const inputRef = useRef(null);
+  const {fieldName, defaultValue, error, registerField} = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value'
+    })
+  }, [fieldName, registerField])
+
   return <Container>
      {Icon && <Icon size={20} />}
-      <input {...props} />
+      <input defaultValue={defaultValue} ref={inputRef} {...props} />
     </Container>
 }
 
