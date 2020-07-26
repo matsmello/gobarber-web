ff642f8d15af0e3e4ea78ae2d42c160d2019c3bd
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-26 01:13:46 -0300

    Save user and token on local storage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,15 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { createContext } from "react";
 import api from "./../services/api";
 
 interface Credentials {
   password: string;
   email: string;
+}
+
+interface AuthState {
+  token: string;
+  user: object;
 }
 
 interface AuthContextData {
@@ -15,13 +20,28 @@ interface AuthContextData {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem("@GoBarber:token");
+    const user = localStorage.getItem("@GoBarber:user");
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }): Promise<void> => {
     const response = await api.post("sessions", {
       email,
       password,
     });
 
-    console.log("response", response);
+    const { token, user } = response.data;
+
+    localStorage.setItem("@GoBarber:token", token);
+    localStorage.setItem("@GoBarber:user", JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
