99ca06668b7f73b8d141f13d2fae24fc62b60fc1
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-26 12:16:08 -0300

    Create auth navigation

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useAuth } from "../hooks/auth";
+import { RouteProps as ReactDOMRouteProps, Redirect, Route as ReactDOMRoute } from "react-router-dom";
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+    {...rest}
+      render={({location}) => {
+        return isPrivate === !!user ?  <Component /> : ( <Redirect to={{pathname: isPrivate ? '/' : '/dashboard', state: {from: location} }} />)}}
+    />
+  )
+}
+
+export default Route;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
+import Route from './Routes';
 
 import SignUp from "./../pages/SignUp";
 import SignIn from "./../pages/SignIn";
+import Dashboard from "../pages/Dashboard";
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
